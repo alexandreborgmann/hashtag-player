afdab5b2f0c06d20a656682ce1067758ab8a2e12
Alexandre Borgmann <40210587+alexandreborgmann@users.noreply.github.com>
2023-03-11 20:37:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function initializeSong() {
 
 cover.src = `imagens/${sortedPlaylist[index].file}.jpg`;
 //song.src = `songs/${sortedPlaylist[index].file}.mp3`;
- song.src = `https://alexandreborgmann.github.io/hashtag-player/songs/Mindme-NoGood.mp3`;   
+ song.src = `songs/Mindme-NoGood.mp3`;   
 songName.innerText = sortedPlaylist[index].songName;
 bandName.innerText = sortedPlaylist[index].artist;
 likeButtonRender();
